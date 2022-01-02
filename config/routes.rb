1c219fa57996657772e6347f287f6c8e88a1d1f6
caroline223 <67647836+caroline223@users.noreply.github.com>
2022-01-02 06:26:11 -0500

    Added the appropriate routes to the app

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
   
+  resources :users
+  resources :contents
+  resources :entries
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
-  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
+  # get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
+
+  get '/me', to: "users#show"
+  post '/signup', to: "users#create"
+
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
 end
