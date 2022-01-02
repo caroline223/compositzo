a24db79d643e52bf073a6c13c40524aeb967a0cc
caroline223 <67647836+caroline223@users.noreply.github.com>
2022-01-02 12:25:07 -0500

    Minort updates

--- client/src/components/authorized_components/EntryPage.js
+++ client/src/components/authorized_components/EntryPage.js
@@ -1,41 +1,41 @@
 import React, { useState, useEffect } from 'react';
 import { Menu, Dropdown, Button, Icon, Table, Card } from 'semantic-ui-react';
-import EntryInfo from './entries_page_components/EntryInfo'
+import EntryInfo from './entries_page/EntryInfo'
 
 
 
 
-function EntryPage({ setUser }){
+function EntryPage(){
 
-    const [entries, setEntries] = useState([])
-    const [filteredEntries, setFilteredEntries] = useState([])
-    const [searchInput, setSearchInput] = useState('')
+    // const [entries, setEntries] = useState([])
+    // const [filteredEntries, setFilteredEntries] = useState([])
+    // const [searchInput, setSearchInput] = useState('')
 
-    useEffect(() => {
-        fetch('/entries')
-        .then(response => response.json())
-        .then((data) => setEntries(data))
-    }, [])
+    // useEffect(() => {
+    //     fetch('/entries')
+    //     .then(response => response.json())
+    //     .then((data) => setEntries(data))
+    // }, [])
 
-    const searchEntries = (event) => {
-        setSearchInput(event.target.value)
-        if(event.target.value !== ''){
-            const filteredData = entries.filter(entry => entry.title.toLowerCase().includes(searchInput.toLowerCase()))
-            setFilteredEntries(filteredData)
-        } else {
-           setFilteredEntries(entries)
-        }
-    }
+    // const searchEntries = (event) => {
+    //     setSearchInput(event.target.value)
+    //     if(event.target.value !== ''){
+    //         const filteredData = entries.filter(entry => entry.title.toLowerCase().includes(searchInput.toLowerCase()))
+    //         setFilteredEntries(filteredData)
+    //     } else {
+    //        setFilteredEntries(entries)
+    //     }
+    // }
 
-    const entryDisplay = () => entries.map((entry) => <EntryInfo key={entry.id} entry={entry} />)
-    const filteredEntryDisplay = () => filteredEntries.map((entry) => <EntryInfo key={entry.id} entry={entry} />)
+    // const entryDisplay = () => entries.map((entry) => <EntryInfo key={entry.id} entry={entry} />)
+    // const filteredEntryDisplay = () => filteredEntries.map((entry) => <EntryInfo key={entry.id} entry={entry} />)
 
     return(
         <div>
            <Menu id='journalMenu' >
                 <Menu.Item position="right" style={{color: 'white', fontWeight: 'bold'}}>Compositzo</Menu.Item>
                 <Menu.Menu position='right'>
-                    <Dropdown item text="Name" setUser={setUser} style={{color: 'white', fontWeight: 'bold'}}>
+                    <Dropdown item text="Name"  style={{color: 'white', fontWeight: 'bold'}}>
                         <Dropdown.Menu>
                             <Dropdown.Item>Account</Dropdown.Item>
                             <Dropdown.Item>Log Out</Dropdown.Item>
@@ -46,7 +46,7 @@ function EntryPage({ setUser }){
               <div style={{backgroundColor: 'gainsboro', textAlign: 'center'}}>
                             <div className="ui search searchBar" >
                                 <div className="ui icon input" >
-                                    <input onChange={searchEntries} placeholder="Search Title" className="prompt"  />
+                                    <input  placeholder="Search Title" className="prompt"  />
                                     <i className="search icon" />
                                 </div>
                             </div>   
@@ -65,10 +65,10 @@ function EntryPage({ setUser }){
                       </Table.Row>
                   </Table.Header>
               </Table>
-             
+{/*              
              <Card.Group itemsPerRow={3}>
                  {filteredEntries.length > 0 ? filteredEntryDisplay(): entryDisplay()}
-             </Card.Group>
+             </Card.Group> */}
         </div>
     )
 }
