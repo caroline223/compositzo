c3936bbce7ed4df6d1a7e83bfe8def3a430c544c
caroline223 <67647836+caroline223@users.noreply.github.com>
2022-01-02 05:12:52 -0500

    Added the features to the EntryPage.js file

---
+++ client/src/components/authorized_components/EntryPage.js
@@ -0,0 +1,75 @@
+import React, { useState, useEffect } from 'react';
+import { Menu, Dropdown, Button, Icon, Table, Card } from 'semantic-ui-react';
+import EntryInfo from './entries_page_components/EntryInfo'
+
+
+
+
+function EntryPage({ setUser }){
+
+    const [entries, setEntries] = useState([])
+    const [filteredEntries, setFilteredEntries] = useState([])
+    const [searchInput, setSearchInput] = useState('')
+
+    useEffect(() => {
+        fetch('/entries')
+        .then(response => response.json())
+        .then((data) => setEntries(data))
+    }, [])
+
+    const searchEntries = (event) => {
+        setSearchInput(event.target.value)
+        if(event.target.value !== ''){
+            const filteredData = entries.filter(entry => entry.title.toLowerCase().includes(searchInput.toLowerCase()))
+            setFilteredEntries(filteredData)
+        } else {
+           setFilteredEntries(entries)
+        }
+    }
+
+    const entryDisplay = () => entries.map((entry) => <EntryInfo key={entry.id} entry={entry} />)
+    const filteredEntryDisplay = () => filteredEntries.map((entry) => <EntryInfo key={entry.id} entry={entry} />)
+
+    return(
+        <div>
+           <Menu id='journalMenu' >
+                <Menu.Item position="right" style={{color: 'white', fontWeight: 'bold'}}>Compositzo</Menu.Item>
+                <Menu.Menu position='right'>
+                    <Dropdown item text="Name" setUser={setUser} style={{color: 'white', fontWeight: 'bold'}}>
+                        <Dropdown.Menu>
+                            <Dropdown.Item>Account</Dropdown.Item>
+                            <Dropdown.Item>Log Out</Dropdown.Item>
+                        </Dropdown.Menu>
+                    </Dropdown>
+                </Menu.Menu>
+              </Menu>  
+              <div style={{backgroundColor: 'gainsboro', textAlign: 'center'}}>
+                            <div className="ui search searchBar" >
+                                <div className="ui icon input" >
+                                    <input onChange={searchEntries} placeholder="Search Title" className="prompt"  />
+                                    <i className="search icon" />
+                                </div>
+                            </div>   
+              </div>
+              <br /><br />
+              <div style={{textAlign: 'center'}}>
+                    <Button color='blue' href="/journal-page">New Entry&nbsp;&nbsp;&nbsp;&nbsp;
+                                <Icon name='plus circle' />
+                    </Button>
+              </div>
+              <br />
+              <Table color='red'>
+                  <Table.Header>
+                      <Table.Row>
+                          <Table.HeaderCell textAlign='center'>Entries</Table.HeaderCell>
+                      </Table.Row>
+                  </Table.Header>
+              </Table>
+             
+             <Card.Group itemsPerRow={3}>
+                 {filteredEntries.length > 0 ? filteredEntryDisplay(): entryDisplay()}
+             </Card.Group>
+        </div>
+    )
+}
+export default EntryPage
