7cbd6b5a93eaa90fd69f560610c3ab88329bb03d
caroline223 <67647836+caroline223@users.noreply.github.com>
2022-01-08 00:24:19 -0500

    Edited the titles of the journal pages on the App.css file

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React, { useState, useEffect } from 'react'
-import { BrowserRouter as Router} from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
 import UnauthorizedUser from './components/UnauthorizedUser';
 import AuthorizedUser from './components/AuthorizedUser';
 
