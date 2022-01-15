ec2b779faf77d1e1ce4477a65ea09608c0fd0c7b
caroline223 <67647836+caroline223@users.noreply.github.com>
2022-01-15 14:47:28 -0500

    Edited the validations for the title within the entry model

--- app/models/content.rb
+++ app/models/content.rb
@@ -1,9 +1,5 @@
 class Content < ApplicationRecord
     belongs_to :entry
 
-    validates :mood, presence: true, allow_blank: false  
-    validates :comment, presence: true, allow_blank: false, length: { 
-        maximum: 1000,
-        too_long: "%{count} characters is the maximum allowed." 
-    }
+   
 end
--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,10 +1,14 @@
 class Entry < ApplicationRecord
-    has_many :contents
     belongs_to :user 
 
     validates :date, presence: true, allow_blank: false 
     validates :title, presence: true, uniqueness: true, allow_blank: false, length: {
-        maximum: 20,
-        too_long: "allows a maxmimum of %{count} characters."
+        minimum: 10,
+        too_short: "has a minimum of %{count} characters."
     }  
+    validates :mood, presence: true, allow_blank: false  
+    validates :content, presence: true, allow_blank: false, length: { 
+        maximum: 1000,
+        too_long: "%{count} characters is the maximum allowed." 
+    }
 end
