760415417640d187cca104e40fb343ec39a22545
caroline223 <67647836+caroline223@users.noreply.github.com>
2022-01-18 03:29:13 -0500

    Edited the validations of the username for the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
 
     validates :fullname, presence: true, uniqueness: false, allow_blank: false
     validates :birthdate, presence: true, uniqueness: false, allow_blank: false
-    validates :username, presence: true, uniqueness: true, length: { maximum: 15 }
+    validates :username, presence: true, uniqueness: true, length: { in: 6..30 }
     validates :email, presence: true, uniqueness: true, allow_blank: false 
     validates :password, presence: true, length: { in: 6..30 }
 end
