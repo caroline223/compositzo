8762d21ef6dadf0aa78d5c4925f0646ba691a779
caroline223 <67647836+caroline223@users.noreply.github.com>
2022-01-17 22:53:57 -0500

    Added the fullname and birthdate categories to the user_params method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -35,7 +35,7 @@ class UsersController < ApplicationController
     end
 
     def user_params
-        params.permit(:username, :password, :password_confirmation, :email)
+        params.permit(:username, :password, :password_confirmation, :email, :fullname, :birthdate)
     end
 
 
