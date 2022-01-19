b6b0f9aab8a09dbc106a89d1be1be3c6d27a32da
caroline223 <67647836+caroline223@users.noreply.github.com>
2022-01-19 11:24:45 -0500

    Created the AccountDeletionSuccessPage and added the route to the UnauthorizedUser component

---
+++ client/src/components/unauthorized_components/AccountDeletionSuccess.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function AccountDeletionSuccess() {
+
+    return(
+        <div>
+            <div className="accountImage">
+                <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /><br /><br />
+                    <h2  style={{textAlign: 'center', color: 'gray', fontWeight: 'bolder'}}>You have successfully deleted your account. Please proceed home.</h2>
+                <br /><br /><br />
+                    <p style={{textAlign: 'center', fontFamily: 'cursive', opacity: '0.7'}}><a className="btn btn-lg btn-danger" href="/" >Home</a></p>
+                <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /><br />
+            </div>
+        </div>
+    )
+
+
+}
+export default AccountDeletionSuccess
