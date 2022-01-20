8bdff750e0531787ed76a6d62275da5adce0c016
caroline223 <67647836+caroline223@users.noreply.github.com>
2022-01-20 10:28:26 -0500

    Updated validations for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
     has_secure_password
     has_many :entries
 
-    validates :fullname, presence: true, uniqueness: false, allow_blank: false
+    validates :fullname, presence: true, uniqueness: false, allow_blank: false, length: { in: 6..30 }
     validates :birthdate, presence: true, uniqueness: false, allow_blank: false
     validates :username, presence: true, uniqueness: true, length: { in: 6..30 }
     validates :email, presence: true, uniqueness: true, allow_blank: false 
